15cd49c43c9511c5ca7c10c31d40bb6b455a9631
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-10 14:20:16 +0200

    feat: Add AutocompleteInput component for enhanced user input
    - Introduced a new AutocompleteInput component to provide users with suggestions while typing.
    - Integrated the Field component for improved accessibility and error handling.
    - Utilized React's useId for unique datalist IDs, enhancing the component's functionality.

---
+++ src/components/autocomplete-input.tsx
@@ -0,0 +1,62 @@
+import type { ComponentProps, InputHTMLAttributes } from 'react';
+import { cn } from '@maxigarcia/js-utils';
+import { useId } from 'react';
+import { Field } from './field';
+
+interface AutocompleteInputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'>,
+  Omit<ComponentProps<typeof Field>, 'children'> {
+  onChange: (value: string) => void;
+  suggestions?: ReadonlyArray<string>;
+  className?: string;
+}
+
+export function AutocompleteInput({
+  value,
+  onChange,
+  suggestions = [],
+  className,
+  error,
+  label,
+  name,
+  id,
+  ...props
+}: AutocompleteInputProps) {
+  const datalistId = useId();
+  const hasSuggestions = suggestions.length > 0;
+
+  return (
+    <Field
+      className={
+        cn(
+          'relative appearance-none rounded border border-gray-600 text-sm focus:border-gray-500',
+          className,
+        )
+      }
+      label={label}
+      name={name}
+      id={id}
+      error={error}
+    >
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => {
+          onChange(event.target.value);
+        }}
+        list={hasSuggestions ? datalistId : undefined}
+        className="h-full w-full p-2 outline-none"
+        {...props}
+      />
+      {hasSuggestions
+        ? (
+            <datalist id={datalistId}>
+              {suggestions.map((suggestion) => (
+                <option key={suggestion} value={suggestion} />
+              ))}
+            </datalist>
+          )
+        : null}
+    </Field>
+  );
+}
