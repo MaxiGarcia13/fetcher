8400fe8761c05c54311b5ca4e9378986a525e204
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-10 11:39:35 +0200

    feat: Enhance RequestEditor with method selection functionality
    - Integrated RequestMethodSelect component to allow users to select HTTP methods.
    - Added state management for the selected method using React's useState hook.
    - Improved the overall user interface of the RequestEditor component.

--- src/components/request-editor/request-editor.tsx
+++ src/components/request-editor/request-editor.tsx
@@ -1,13 +1,18 @@
+import type { HttpMethod } from './request-method-select';
 import { cn } from '@maxigarcia/js-utils';
+import { useState } from 'react';
+import { RequestMethodSelect } from './request-method-select';
 
 interface Props {
   className?: string;
 }
 
 export function RequestEditor({ className }: Props) {
+  const [method, setMethod] = useState<HttpMethod>('GET');
+
   return (
     <div className={cn('flex gap-4', className)}>
-      <h1>Request Editor</h1>
+      <RequestMethodSelect value={method} onChange={setMethod} />
     </div>
   );
 }
