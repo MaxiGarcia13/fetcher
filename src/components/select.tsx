c66ae8f6faa23f36f41d03ac9b2669b3d1483f7e
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-10 11:39:25 +0200

    feat: Add Select component for improved form handling
    - Introduced a new Select component to facilitate user selection from a list of options.
    - The component supports custom styling and integrates the ChevronDownIcon for enhanced UI interaction.
    - Implemented onChange handling to manage selected values effectively.

---
+++ src/components/select.tsx
@@ -0,0 +1,47 @@
+import { cn } from '@maxigarcia/js-utils';
+import { ChevronDownIcon } from './icons/chevron-down';
+
+export interface SelectOption {
+  label: string;
+  value: NativeSelectProps['value'];
+}
+
+type NativeSelectProps = React.SelectHTMLAttributes<HTMLSelectElement>;
+
+interface SelectProps extends Omit<NativeSelectProps, 'onChange'> {
+  onChange: (value: NativeSelectProps['value']) => void;
+  options: ReadonlyArray<SelectOption>;
+  className?: string;
+}
+
+export function Select({
+  value,
+  onChange,
+  options,
+  className,
+  ...props
+}: SelectProps) {
+  return (
+    <div className="relative inline-flex items-center">
+      <select
+        value={value}
+        onChange={(event) => {
+          onChange(event.target.value);
+        }}
+        className={cn(
+          'border border-gray-600 focus:border-gray-600',
+          'h-10 appearance-none rounded-md  py-2 pr-9 pl-3 text-sm outline-none',
+          className,
+        )}
+        {...props}
+      >
+        {options.map((option) => (
+          <option key={option.value.toString()} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <ChevronDownIcon className="pointer-events-none absolute right-3 size-4 text-gray-600" />
+    </div>
+  );
+}
