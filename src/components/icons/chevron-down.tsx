2f4d84f80a30d02b7ae50076c88393d1ff205c4c
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-10 11:39:17 +0200

    feat: Add ChevronDownIcon component for enhanced UI
    - Introduced a new ChevronDownIcon component as an SVG for improved scalability and styling flexibility.
    - This component can be utilized in various parts of the application to enhance user interaction.

--- src/components/icons/chevron-down.tsx
+++ src/components/icons/chevron-down.tsx
@@ -1,4 +1,4 @@
-export default function SearchIcon({ className }: { className: string }) {
+export function ChevronDownIcon({ className }: { className: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -13,8 +13,7 @@ export default function SearchIcon({ className }: { className: string }) {
       className={className}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-      <path d="M21 21l-6 -6" />
+      <path d="M6 9l6 6l6 -6" />
     </svg>
   );
 }
