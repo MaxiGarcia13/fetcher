29dc4f5537ca4f1aee9d39f5efe6513e96516baf
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-12 12:45:46 +0200

    feat: Update DocButton to utilize HTTP request state and disable button based on URL availability
    - Imported useHttpRequestState to access the current URL state.
    - Disabled the DocButton when the URL is not available, improving user experience.

--- src/components/doc-button.tsx
+++ src/components/doc-button.tsx
@@ -1,15 +1,18 @@
+import { useHttpRequestState } from '@/store/http-request';
 import { Button } from './button';
 import { DocIcon } from './icons/doc';
 import { Tooltip } from './tooltip';
 
 export function DocButton({ className }: { className?: string }) {
+  const { url } = useHttpRequestState();
+
   const handleClick = () => {
     window.location.href = `/doc${window.location.search}`;
   };
 
   return (
     <Tooltip content="Documentation" position="bottom" className={className}>
-      <Button onClick={handleClick} className={className}>
+      <Button onClick={handleClick} className={className} disabled={!url}>
         <DocIcon className="size-4" />
       </Button>
     </Tooltip>
