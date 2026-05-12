98d983ee16a528163d10e82248c99c6cba9c0d48
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-12 12:12:59 +0200

    feat: Enhance ShareButton component with copy functionality and dynamic tooltip
    - Introduced state management to track copy status for the ShareButton.
    - Updated tooltip content based on copy status, displaying 'Link copied' when the link is successfully copied.
    - Refactored button rendering to include dynamic icon and variant based on the copy state.

--- src/components/share-button.tsx
+++ src/components/share-button.tsx
@@ -1,17 +1,55 @@
+import type { ComponentProps, ReactNode } from 'react';
+import { useState } from 'react';
 import { Button } from './button';
+import { CheckIcon } from './icons/check';
 import { LinkIcon } from './icons/link';
 import { Tooltip } from './tooltip';
 
-export function ShareButton({ className }: { className?: string }) {
+interface ShareButtonData {
+  tooltip: string;
+  variant: ComponentProps<typeof Button>['variant'];
+  icon: ReactNode;
+}
+
+export function ShareButton({ className, children }: { className?: string; children?: ReactNode }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  let data: ShareButtonData = {
+    tooltip: 'Share link',
+    variant: 'default',
+    icon: <>
+      <LinkIcon className="size-5" />
+      {' '}
+      {children}
+    </>,
+  };
+
+  if (isCopied) {
+    data = {
+      tooltip: 'Link copied',
+      variant: 'success',
+      icon: <CheckIcon className="size-5" />,
+    };
+  }
+
   const handleShare = () => {
+    setIsCopied(true);
     const url = window.location.href;
     navigator.clipboard.writeText(url);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
   };
 
   return (
-    <Tooltip content="Share link" position="bottom" className={className}>
-      <Button onClick={handleShare} className={className}>
-        <LinkIcon className="size-5" />
+    <Tooltip content={data.tooltip} position="bottom" className={className}>
+      <Button
+        onClick={handleShare}
+        variant={data.variant}
+        className={className}
+      >
+        {data.icon}
       </Button>
     </Tooltip>
   );
