0ac2c0b5c9d3241a1feb59e54d38067f5b2ceb86
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-13 12:52:08 +0200

    feat: Add Tailwind CSS configuration for dark mode and custom font
    - Introduced a new Tailwind CSS configuration file to enable dark mode support and extend the theme with a custom font family, "Fira Code". This enhances the styling capabilities of the project.

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  darkMode: ['class', '[data-theme="dark"]'],
+  theme: {
+    extend: {
+      fontFamily: {
+        fira: ['"Fira Code"', 'ui-monospace', 'monospace'],
+      },
+    },
+  },
+};
