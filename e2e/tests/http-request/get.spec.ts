a41aae9d5cd46caaa854eb3daf9035d118934743
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-13 13:52:32 +0200

    feat: Enhance HTTP request tests with detailed request validation
    - Updated the test for a mocked 200 response to include validation of request headers, parameters, and URL.
    - Improved the structure of the mocked 500 response test for better readability.
    - Ensured consistency in the handling of network error tests by removing unnecessary lines.

--- e2e/tests/http-request/get.spec.ts
+++ e2e/tests/http-request/get.spec.ts
@@ -8,14 +8,41 @@ import { sendRequest } from '../../utils/send-request';
 
 test('shows a mocked 200 response', async ({ page }) => {
   await mockHttpRequestSuccess(page, 200, { message: 'Mocked OK' });
-  await sendRequest(page);
+
+  const expectedRequestHeaders = {
+    'Content-Type': 'application/json',
+  };
+
+  const expectedRequestParameters = {
+    param1: 'value1',
+    param2: 'value2',
+  };
+
+  const requestUrl = 'https://example.test/api/params';
+
+  const response = await sendRequest(page, {
+    method: 'GET',
+    url: requestUrl,
+    params: expectedRequestParameters,
+    headers: expectedRequestHeaders,
+  });
 
   await expect(page.getByTestId(HTTP_REQUEST_TEST_ID.RESPONSE_EDITOR)).toBeVisible();
   await expect(page.getByTestId(HTTP_REQUEST_TEST_ID.RESPONSE_EDITOR)).toContainText('Mocked OK');
+
+  expect(response?.request().postDataJSON()).toEqual({
+    url: requestUrl,
+    method: 'GET',
+    params: expectedRequestParameters,
+    headers: expectedRequestHeaders,
+    body: '{}',
+  });
 });
 
 test('shows a mocked 500 response', async ({ page }) => {
-  await mockHttpRequestSuccess(page, 500, { message: 'Internal Server Error' });
+  await mockHttpRequestSuccess(page, 500, {
+    message: 'Internal Server Error',
+  });
   await sendRequest(page);
 
   await expect(page.getByTestId(HTTP_REQUEST_TEST_ID.RESPONSE_EDITOR)).toBeVisible();
@@ -26,6 +53,7 @@ test('shows a mocked 500 response', async ({ page }) => {
 
 test('shows a mocked network error', async ({ page }) => {
   await mockHttpRequestNetworkError(page);
+
   await sendRequest(page);
 
   await expect(page.getByTestId(HTTP_REQUEST_TEST_ID.RESPONSE_EDITOR)).toBeVisible();
